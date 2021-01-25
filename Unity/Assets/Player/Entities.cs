ea9b9a4411ef26e7049b7e6335767e0cc2406e39
Edgar D <Edgar.delaporte@epita.fr>
2021-01-25 21:58:14 +0100

    Course, saut et classe Entity/Player

---
+++ Unity/Assets/Player/Entities.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* public class EntityScript : MonoBehaviour
+{
+	public class Entity : EntityScript
+    {
+		protected bool alive;
+    	protected float PV;
+    	protected float speed;
+   	 	protected float power;
+
+    	void Start()
+    	{
+        	alive = true;
+    	}
+	}
+
+	public class Player : Entity
+	{
+		protected List<Item> inventory;
+    	protected Weapon _weapon;
+
+		void Start()
+		{
+			inventory = List<Item>();
+        	_weapon = null;
+			PV = 10f;
+        	speed = 6f;
+        	power = 1f;
+		}
+	}
+} */
+
---
+++ Unity/Assets/Player/Jump.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpHeight;
+    public bool isGrounded;
+    public bool jumping = false;
+    public Rigidbody rb;
+    public int JumpCount = 60;
+    public CharacterController controller;
+    
+    void Update()
+    {
+        if (controller.velocity.y == 0)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                jumping = true;
+            }
+        }
+        if (jumping)
+        {
+            rb.useGravity = false;
+            controller.Move(Vector3.up * (jumpHeight/20));
+            jumpHeight -= 0.01f;
+            JumpCount -= 1;
+            if (JumpCount == 0)
+            {
+                jumping = false;
+                rb.useGravity = true;
+                JumpCount = 60;
+                jumpHeight = 1f;
+            }
+        }
+    }
+    
+}
