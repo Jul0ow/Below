61bfddb50243fc59acde772eb105ba7f99d984ef
matthieu <matthieu.colin@epita.fr>
2021-01-26 17:13:56 +0100

    Auto stash before merge of "main" and "origin/main"
    On peut rejoindre une room

--- Unity/Assets/Scripts/Launcher.cs
+++ Unity/Assets/Scripts/Launcher.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
-public class Luncher : MonoBehaviourPunCallbacks
+public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+    
     [SerializeField] TMP_InputField roomNameInputField;
 
     [SerializeField] private TMP_Text errorText;
     [SerializeField] private TMP_Text roomNameText;
+    [SerializeField] private Transform roomListContent;
+    [SerializeField] private GameObject roomListItemPrefab;
+
+    void Awake()
+    {
+        Instance=this;
+    }
     
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Connecting To Master");
@@ -60,8 +69,27 @@ public class Luncher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("loading");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+    
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("title");
     }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+    
 }
