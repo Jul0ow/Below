923e0d673e9426ac0f0617b057cb62642099d858
Diaz Jules <jules.diaz@epita.fr>
2021-02-12 15:59:04 +0100

    Menu options

---
+++ Unity/Assets/Scripts/Multi/Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Options : MonoBehaviour
+{
+    public void options()
+    {
+        PhotonNetwork.LoadLevel(3);
+    }
+
+    public void returnmenu()
+    {
+        PhotonNetwork.LoadLevel(0);
+        PhotonNetwork.Disconnect();
+    }
+    
+    public void sound()
+    {
+        
+    }
+}
