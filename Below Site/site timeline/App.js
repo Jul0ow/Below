01c463456ed993b1390fef4c4ee6c404cb9b60d2
matthieu <matthieu.colin@epita.fr>
2021-04-22 17:56:58 +0200

    ajout de la timeline sur le site

---
+++ Below Site/site timeline/App.js
@@ -0,0 +1,20 @@
+const allRonds = document.querySelectorAll('.rond');
+const allboxes = document.querySelectorAll('.box');
+
+const controller = new ScrollMagic.Controller()
+
+allboxes.forEach(box => {
+  for ( i = 0; i < allRonds.length; i++) {
+    if (allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+      let tween = gsap.from(box,{y: -50, opacity: 0,duration: 0.5}) /*animation*/
+
+      let scene = new ScrollMagic.Scene({
+        triggerElement: allRonds[i],
+        reverse: false /* ne disparait pas quand on scroll vers le haut*/
+      })
+        .setTween(tween)
+        /*.addIndicators()*/ /*ajout des indications sur la droite*/
+        .addTo(controller)
+    }
+  }
+})
