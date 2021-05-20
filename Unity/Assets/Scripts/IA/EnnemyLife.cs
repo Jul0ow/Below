8f408ad576863199c84a04d66b33789445b18480
Diaz Jules <jules.diaz@epita.fr>
2021-05-20 22:36:52 +0200

    remplacement des audio -> audio source + Musique menu qui marche + AudioMixer....

--- Unity/Assets/Scripts/IA/EnnemyLife.cs
+++ Unity/Assets/Scripts/IA/EnnemyLife.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class EnnemyLife : MonoBehaviour
 {
     public EnnemyIA IA;
-    public AudioClip death;
     public int Health;
     
     public virtual void TakeDamage(int damage)
@@ -14,7 +13,7 @@ public class EnnemyLife : MonoBehaviour
         Health -= damage;
         if (Health <= 0)
         {
-            AudioSource.PlayClipAtPoint(death, GetComponent<Transform>().position);
+            GetComponent<AudioSource>().Play();
             DestroyEnemy();
         }
     }
