920334d91d999f558a186dea05d936625e9f59e2
Edgar D <Edgar.delaporte@epita.fr>
2021-06-18 19:02:11 +0200

    débuts du boss

---
+++ Unity/Assets/Scripts/IA/BloodIA.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BloodIA : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject Boss;
+    public int speed;
+    
+    protected virtual void Awake()
+    {
+        agent.speed = speed;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    protected void Update()
+    {
+        agent.SetDestination(Boss.transform.position);
+        transform.LookAt(Boss.transform);
+    }
+}
---
+++ Unity/Assets/Scripts/IA/BossIA.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossIA : EnnemyIA
+{
+    public int Force;
+    public EnnemyLife Life;
+    public GameObject Serie1;
+    public GameObject Summonner;
+    public GameObject Serie2;
+    public GameObject Lava;
+    public GameObject Light;
+    public bool Dead;
+    public Animator animator;
+
+
+    protected override void Update()
+    {
+        if(Dead)
+        {
+            animator.SetBool("Die",true);
+            return;
+        }
+        float distance = float.MaxValue;
+        foreach (var joueur in GameObject.FindGameObjectsWithTag("Cible"))
+        {
+            float newDistance = Vector3.Distance(joueur.transform.position, this.transform.position);
+            if (newDistance < distance)
+            {
+                distance = newDistance;
+            }
+        }
+
+        agent.speed = speed;
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsplayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsplayer);
+
+        if (Life.Health > 16000)
+        {
+            Attackplayer();
+        }
+        
+        else if (Life.Health > 8000)
+        {
+            Serie1.SetActive(false);
+            if (playerInAttackRange)
+            {
+                animator.SetBool("Run forward",false);
+                agent.SetDestination(transform.position);
+                transform.LookAt(player.transform);
+                if (!alreadyAttacked)
+                {
+                    Collider[] enemies = Physics.OverlapSphere(transform.position, 2);
+                    for (int i = 0; i < enemies.Length; i++)
+                    {
+                        if (enemies[i].CompareTag("Player"))
+                        {
+                            enemies[i].GetComponent<CharacterThings>().TakeDamage(0, false, false);
+                            enemies[i].GetComponent<Rigidbody>().AddExplosionForce(Force, transform.position, 3);
+                        }
+                    }
+                    alreadyAttacked = true;
+                    Invoke(nameof(ResetAttack), timeBetweenAttacks);
+                }
+                else
+                {
+                    if (playerInSightRange)
+                    {
+                        animator.SetBool("Run forward",true);
+                        Chaseplayer();
+                    }
+                    else
+                    {
+                        animator.SetBool("Stab Attack",true);
+                        Attackplayer();
+                    }
+                }
+            }
+        }
+        
+        else if (Life.Health > 2000)
+        {
+            animator.SetBool("Run forward",false);
+            animator.SetBool("Stab Attack",false);
+            animator.SetBool("Cast spell",true);
+            Summonner.SetActive(true);
+            Collider[] blood = Physics.OverlapSphere(transform.position, 3);
+            for (int i = 0; i < blood.Length; i++)
+            {
+                if (blood[i].gameObject.GetComponent<BloodIA>())
+                {
+                    Destroy(blood[i]);
+                    Life.Health += 300;
+                }
+            }
+            Attackplayer();
+        }
+
+        else
+        {
+            animator.SetBool("Cast spell",false);
+            animator.SetBool("Jump",true);
+            Summonner.SetActive(false);
+            Serie2.SetActive(true);
+            Light.SetActive(true);
+            Lava.transform.position += new Vector3(0,0.0001f,0);
+            if (Life.Health < 1000) Life.Health += 500;
+            Attackplayer();
+        }
+    }
+}
