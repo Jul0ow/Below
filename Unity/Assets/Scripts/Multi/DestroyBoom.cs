fdbbf6a4aa85a3e62e0fbfd71f757da8b9e3f4f3
Edgar D <Edgar.delaporte@epita.fr>
2021-06-26 19:45:58 +0200

    Détruire les bouboules

---
+++ Unity/Assets/Scripts/Multi/DestroyBoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class DestroyBoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(5);
+        if(PhotonNetwork.IsMasterClient)
+            PhotonNetwork.Destroy(gameObject);
+    }
+}
